f38624402eadefa40cd5cc9aabfc28ced786556e
idosinchuk <idosinchuk@ITEM-S84804.emea.msad.sopra>
2019-10-14 13:50:03 +0200

    Adding logs

--- src/main/java/com/idosinchuk/teagile/gateway/service/GatewayApplication.java
+++ src/main/java/com/idosinchuk/teagile/gateway/service/GatewayApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @EnableEurekaClient // It acts as a eureka client
 @EnableZuulProxy // Enable Zuul
 @SpringBootApplication
@@ -12,6 +15,7 @@ public class GatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
+        log.info("-- GatewayApplication started --");
 	}
 
 }
